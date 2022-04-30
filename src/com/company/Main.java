51459d8616fa3414aca65053d8b127a47950bc5d
whereareyo <104694707+wherear3y0@users.noreply.github.com>
2022-05-01 01:12:37 +0300

    last

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,48 +2,39 @@ package com.company;
 
 import java.util.*;
 
-
-
 //my task: Implement comb sorting. Apply this sort to sort strings according to the following criteria:
 //  - first, according to the number of vowels in a line (for the English alphabet), lines with a large number of vowels should come first;
 //  - then along the length of the line, longer words in front.
 public class Main {
     public static void main(String[] args) {
 
-        int[] myArray = new int[10];
-        myArray[0] = 1;
-        myArray[1] = 3;
-        myArray[2] = 9;
-        myArray[3] = 7;
-        myArray[4] = 5;
-        myArray[5] = 2;
-        myArray[6] = 6;
-        myArray[7] = 4;
-        myArray[8] = 9;
-        myArray[9] = 6;
-
-       String str = "\"Suggestion for the implementation of my assignment about programming\"";
-
-        String[] stroke = {"Suggestion for the implementation of my assignment about programming", "I wanted a beautiful fate, like in the Roots video by Eric Badu Storytelling on the go",
+        String[] myStringArray = {"Suggestion for the implementation of my assignment about programming", "I wanted a beautiful fate, like in the Roots video by Eric Badu Storytelling on the go",
                 "I MC scourged and lynched everyone who rested on their laurels And lunched with their mediocre texts",
                 "If you resist the surrounding whore It's not with a sad face"};
 
+        ArrayList<String> myStringArray1 = new ArrayList<>(Arrays.asList(myStringArray));
 
-        str = str.toLowerCase();// create stroke with lowercase words
-        String[] words = str.split(" "); //split the string around the spaces and put it directly into the array
-       // string of vowels
-
-        System.out.println("Array with numbers for example");
-        System.out.println("Array before sort: " + Arrays.toString(myArray));
-        Sort(myArray);
-
-        System.out.println("Stroke before sort: " + str);
-        SortToString(words);
+        System.out.println("My task with words");
+        System.out.println("Array before sort: ");
+        for(int i = 0; i < myStringArray.length; i++){
+        System.out.println(myStringArray[i]);}
+        System.out.println("-------------------------------------");
+        solve(myStringArray1);
     }
-    //поросто для чисел
-    public static void Sort(int [] myArray) {
-        int gap = myArray.length;
 
+    public static void solve(ArrayList<String> myStringArray){
+
+        for (int i = 0; i < myStringArray.size(); i++){
+            myStringArray.set(i, str(myStringArray.get(i)));
+        }
+        SortToString(myStringArray);
+        System.out.println("Array after sort: ");
+        for(int i =0; i < myStringArray.size(); i++){
+        System.out.println( myStringArray.get(i));}
+    }
+    public static void SortToString(ArrayList<String> myStringArray){
+        //сортировка расческой
+        int gap = myStringArray.size();
         boolean isSorted = false;
         while(!isSorted || gap != 1){
             if(gap > 1){
@@ -54,54 +45,47 @@ public class Main {
             }
 
             isSorted = true;
-            for(int i = gap; i < myArray.length; i++){
-                if(myArray[i - gap] > myArray[i] ){
-                    int tmp = myArray[i];
-                    myArray[i] = myArray[i - gap];
-                    myArray[i - gap] = tmp;
+            for(int i = gap; i < myStringArray.size(); i++){
+                if(sorted(myStringArray.get(i - gap)) < sorted(myStringArray.get(i))){
+                    String tmp = myStringArray.get(i);
+                    myStringArray.set(i, myStringArray.get(i - gap));
+                    myStringArray.set(i - gap, tmp);
                     isSorted = false;
                 }
             }
         }
-        System.out.println("Array after sort: " + Arrays.toString(myArray));
-        System.out.println("-------------------------------------------------");
+
     }
-    public static void SortToString(String[] words){
-
-        //comb sort
-        int gap = words.length;
-
-        boolean isSorted = false;
-        while(!isSorted || gap != 1){
-            if(gap > 1){
-                gap = gap * 10 / 13;
-
-            }else{
-                gap = 1;
-            }
-
-            isSorted = true;
-            for(int i = gap; i < words.length; i++){
-                if(FindVowel(words[i - gap]) < FindVowel(words[i])){
-                    String tmp = words[i];
-                    words[i] = words[i - gap];
-                    words[i - gap] = tmp;
-                    isSorted = false;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(words));
-    }
-    public static int FindVowel(String word){
-        int k = 0;
+    public static int sorted(String pattern){
         String vowel = "aeiouy";
-        for(int i = 0; i < word.length(); i++){
-            for(int j = 0; j < vowel.length(); j++){
-                if(word.charAt(i) == vowel.charAt(j)){
+        int k = 0;
+        for (int i = 0; i < pattern.length(); i++){
+            for (int j = 0; j < vowel.length(); j++){
+                if (pattern.charAt(i) == (vowel.charAt(j))){
                     k++;
                 }
             }
         }
         return k;
     }
+    public static String str(String pattern){
+        String simbol = "[-.?!)(,:] ";
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < pattern.length(); i++){
+            sb.append(pattern.charAt(i));
+            for (int j = 0; j < simbol.length(); j++){
+                if (pattern.charAt(i) == (simbol.charAt(j))){
+                    sb.deleteCharAt(sb.length()-1);
+                    list.add(sb.toString());
+                    sb.setLength(0);
+                }
+            }
+        }
+        list.add(sb.toString());
+        SortToString(list);
+        return String.join(" ", list);
+    }
+
 }
